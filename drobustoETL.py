334be9fd5db8baed839711ae33b792a477b4fa3a
darobusto <darobusto@gmail.com>
2020-09-18 20:47:14 -0400

    final commit

--- drobustoETL.py
+++ drobustoETL.py
@@ -7,8 +7,12 @@ from sqlalchemy import create_engine
 
 # New Jersey, can be anything
 state = '34'
-variables = ['B01001_001E', 'B01002_001E', 'B19013_001E']
-variable_names = ['pop', 'med_age', 'med_income']
+variables = ['B01001_001E', 'B01002_001E', 'B19013_001E', 'B02001_002E', 'B02001_003E', 'B03003_001E',
+             'B11001_002E', 'B11001_007E', 'B15003_017E', 'B15003_022E',
+             'B15003_023E', 'B15003_025E', 'B25064_001E']
+variable_names = ['pop', 'med_age', 'med_income', 'race_white', 'race_black', 'origin_hisp_latino',
+                  'family_households', 'non_family_households', 'education_HS', 'education_bachelors',
+                  'education_masters', 'education_doctorate', 'med_rent']
 
 # Pulls a geo object containing all counties for the 'state' variable
 counties_geo = censusdata.geographies(censusdata.censusgeo([('state', state), ('county', '*')]), 'acs5', 2018, key = '3d7e71138248eccfa1f372adc09ca6240ab16dd1')
@@ -21,25 +25,15 @@ for county in counties_geo:
     geo_tuple = geo_tuple[1][1]
     countyList.append(geo_tuple)
 
-print(countyList)
-
-
 # Working code to get info on each of those tables for all counties & Block Groups
-
 block_groups = pd.DataFrame()
 
-'''
+
 for county in countyList:
     test = censusdata.download('acs5', 2018,
                                censusdata.censusgeo([('state', '34'), ('county', '{county}'.format(county = county)), ('block group', '*')]), variables,
                                key = '3d7e71138248eccfa1f372adc09ca6240ab16dd1')
     block_groups = block_groups.append(test)
-    #print(block_groups)
-'''
-
-block_groups = censusdata.download('acs5', 2018,
-                               censusdata.censusgeo([('state', '34'), ('county', '001'.format(county = county)), ('block group', '*')]), variables,
-                               key = '3d7e71138248eccfa1f372adc09ca6240ab16dd1')
 
 # Set the column names
 block_groups.columns = variable_names
@@ -63,44 +57,23 @@ block_groups.reset_index(inplace=True)
 block_groups.drop(columns='index', inplace=True)
 
 # Regex our columns into what we want
-
 block_groups['block_group'] = block_groups['block_group'].str.extract('(\d+)', expand = False)
 block_groups['census_tract'] = block_groups['census_tract'].str.extract('(\d+)', expand = False)
 block_groups['state'] = block_groups['state'].str.extract('(^[^:]+\s*)', expand = False)
 
-print(block_groups)
+# Print the dataframe if you want
+# print(block_groups)
 
-block_groups.to_csv('block_groups.csv', index=False)
+# Print to a csv if you want
+# block_groups.to_csv('block_groups.csv', index=False)
 
+# Pass command line argument to be used with your secret info
 db_string = sys.argv[1]
 
+# Set the same for the table
 table_name = 'drobusto_etl'
 
+# Create the database engine
 engine = create_engine(db_string)
+# Copy the table into the database
 block_groups.pg_copy_to(table_name, engine, if_exists = 'replace', index = False, schema = 'acs')
-
-
-# We want to split off the index into a column, split that column on ,s
-# Make four columns "block group" "census tract" "county" "state"
-# For the last two, just grabbing the value is fine
-# For the first two, we just want the value to be the last character
-
-# We have a nasty geo object but we can probably loop through, convert it to a string, then
-# just cut off after from the : to the end
-
-###########
-# Need to get credentials from pgpass file to get into the database
-# Either have the script read directly from that file
-# OR make another credentials file that's formatted better
-
-# psychopg2 allows you to run sql that pulls data into the database
-# copy csv to postgress with psychopg2
-
-# Commit file to github then pull it into the server
-# or scp which is more lightweight
-
-# Ohio pandas extension
-
-
-
-
